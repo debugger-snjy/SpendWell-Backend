b7ca18e3862a07d89d0729f4cad0f9ea6b7bb4e7
Sanjay Sukhwani <sanjayasukhwani@gmail.com>
2025-07-13 15:00:43 +0530

    Update database connection to use environment variable and upgrade dotenv package

--- Database/Connection.js
+++ Database/Connection.js
@@ -1,19 +1,22 @@
 // Importing the Mongoose package
 const mongoose = require('mongoose');
 
+// Importing the dotenv for JWT_SECRET_KEY access
+const dotenv = require('dotenv');
+
+// Getting the Environment Variables from the env file
+dotenv.config();
+
 // Defining the Mongo URI for Database
 // Now, Defining the Database of SpendWell
 
-// For Online Mongo Atlas
-// const mongoURI = "mongodb+srv://dbUser:dbUserPassword@dbcluster.pqt7lhm.mongodb.net/SpendWell";
-
 // For Local MongoDB
-const mongoURI = "mongodb://127.0.0.1:27017/SpendWell";
+//const mongoURI = "mongodb://127.0.0.1:27017/SpendWell";
 
 // Checking whether the node js is connected to mongoose or not
 const connectToMongoCluster = async () => {
     try {
-        const db = await mongoose.connect(mongoURI);
+        const db = await mongoose.connect(process.env.MONGO_URI);
         console.log("Connection Successfull !");
         return db;
     } catch (error) {
